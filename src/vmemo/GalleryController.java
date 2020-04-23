165f9f9ce09baea2a58c8cb8fc36d3ed283dcf1d
dutt patel <djp5735@psu.edu>
2020-04-23 17:56:26 -0400

    Refactoring code

--- src/vmemo/GalleryController.java
+++ src/vmemo/GalleryController.java
@@ -9,8 +9,25 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * 
+ * @author Dutt2
+ */
+
 public class GalleryController {
     
+       @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+   @FXML // URL location of the FXML file that was given to the FXMLLoader
+  private URL location;
+    
+   
+   @FXML // This method is called by the FXMLLoader when initialization is complete
+   void initialize() {
+
+   }
+   
     @FXML
     private Label controller2;
     
@@ -26,16 +43,3 @@ public class GalleryController {
         model.setView(0);
     }
 }
-    
-
-  //  @FXML // ResourceBundle that was given to the FXMLLoader
-    //private ResourceBundle resources;
-
-  ///\  @FXML // URL location of the FXML file that was given to the FXMLLoader
-   // private URL location;
-
-   // @FXML // This method is called by the FXMLLoader when initialization is complete
-  //  void initialize() {
-
-   // }
-//}
